989653f40514323433314ee570f188a0353d293e
Armando Lima <armandomattei1997@gmail.com>
2021-05-10 08:18:02 -0300

    Creating actions to capture the value of the slider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //OUTLETS
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func heightSliderChanged(_ sender: Any) {
+        
+        print(heightSlider.value)
+        
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: Any) {
+        
+        print(weightSlider.value)
+        
+    }
+    
+    
 
 
 }
