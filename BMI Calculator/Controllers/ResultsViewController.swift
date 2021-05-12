58966cf43adcf969c171305abac58190d49ec9eb
Armando Lima <armandomattei1997@gmail.com>
2021-05-12 08:54:18 -0300

    Removing self on dismiss

--- BMI Calculator/Controllers/ResultsViewController.swift
+++ BMI Calculator/Controllers/ResultsViewController.swift
@@ -15,7 +15,7 @@ class ResultsViewController: UIViewController {
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
      
-        self.dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
         
         
     }
